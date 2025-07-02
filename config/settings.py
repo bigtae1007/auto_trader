360db33ad44edd5f3a78ee2952d815ab81da34ae
박태형 <btae1007@gmail.com>
2025-07-03 00:52:18 +0900

    env환경 생성

--- config/settings.py
+++ config/settings.py
@@ -1,8 +1,20 @@
 # config/settings.py
 
-# ⚠️ 진짜 키가 아닌 테스트용 임시 키
-BINANCE_API_KEY = "your_api_key_here"
-BINANCE_SECRET_KEY = "your_secret_key_here"
+import os
+from dotenv import load_dotenv
+
+# 1. 환경명 가져오기 (운영체제 환경변수 or 기본값)
+ENV = os.getenv("ENV", "dev")
+
+# 2. env 파일 경로 설정
+env_file = f".env.{ENV}"
+
+# 3. 해당 파일 로딩
+load_dotenv(dotenv_path=env_file)
+
+RUN_MODE = os.getenv("RUN_MODE", "development")
+BINANCE_API_KEY = os.getenv("BINANCE_API_KEY")
+BINANCE_SECRET_KEY = os.getenv("BINANCE_SECRET_KEY")
 
 # 거래 기본 설정
 DEFAULT_SYMBOL = "BTCUSDT"  # 기본 거래 코인
